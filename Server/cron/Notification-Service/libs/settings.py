a9ab8e1cecd1872a869bac5929dbf11f517ceec3
mike barta <mikba@mikes-MacBook-Pro.local>
2015-02-10 12:35:35 -0800

    changed to leelander dev keys

--- Server/cron/Notification-Service/libs/settings.py
+++ Server/cron/Notification-Service/libs/settings.py
@@ -1,4 +1,5 @@
-AIRSHIPKEY = "pkJbimr6QsmRArbPsZP7EA"
-AIRSHIPSEC = "rv5cWFQUQK-oq7QILZyGPw"
+# development keys, change to production after testing
+AIRSHIPKEY = "cMs-efMTQleCkr2iHKDc7Q"
+AIRSHIPSEC = "DYM8-ISySE25afFQoH2tMg"
 
 INSECTURL = "http://www.insectforecast.com/api/korkmaz/get_recent_posts/?post_type=bug&count=3"
